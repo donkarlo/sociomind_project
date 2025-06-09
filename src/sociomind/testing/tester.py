1e88810bb83169a3d1a0563f938b22138baa9749
Mohammad Rahmani <mohammad.rahmani.xyz@gmail.com>
2025-06-09 18:57:13 +0200

    trainer structured

--- src/sociomind/testing/tester.py
+++ src/sociomind/testing/tester.py
@@ -1,5 +1,11 @@
+from typing import Union
+
+from utilityx.data.source import Source
+from utilityx.osx.os_path import OsPath
+
+
 class Tester:
-    def __init__(self, learning_source:Union[str, OsPath, DataSource]):
+    def __init__(self, learning_source:Union[str, OsPath, Source]):
         pass
 
     def read_sensor_data_one_by_one(self):
