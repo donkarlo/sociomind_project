26ca4bae8f7b94e64af3b3f850d92de07bd27533
Mohammad Rahmani <mohammad.rahmani.xyz@gmail.com>
2025-06-10 23:08:53 +0200

    tester

--- src/sociomind/testing/tester.py
+++ src/sociomind/testing/tester.py
@@ -5,7 +5,7 @@ from utilityx.osx.os_path import OsPath
 
 
 class Tester:
-    def __init__(self, learning_source:Union[str, OsPath, Source]):
+    def __init__(self, models:list[]):
         pass
 
     def read_sensor_data_one_by_one(self):
