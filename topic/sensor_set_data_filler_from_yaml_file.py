b5469c7ac330742ef84f0aa3ec6149b64443c77f
Mohammad Rahmani <mohammad.rahmani.xyz@gmail.com>
2025-06-20 22:47:52 +0200

    reading from yaml

---
+++ topic/sensor_set_data_filler_from_yaml_file.py
@@ -0,0 +1,9 @@
+
+from sensorx.sensor_set import SensorSet
+import yaml
+from yaml import CLoader
+
+
+class SensorSetDataFillerFromMixedYaml():
+    def __init__(self, yaml_file:DataSource):
+        pass
