7edfcd9414bed9fccb3e1198109f00a8f9e4b54e
Mohammad Rahmani <mohammad.rahmani.xyz@gmail.com>
2025-08-09 23:06:16 +0200

    experiment update

--- src/sociomind/experiments/experiment.py
+++ src/sociomind/experiments/experiment.py
@@ -4,7 +4,7 @@ from src.sociomind.experiments.scenario import Scenario
 class Experiment:
     def __init__(self, learning_scenario:Scenario , test_scenarios: tuple[Scenario,...]):
         self._test_scenarios = test_scenarios
-        self._learning_scenario = learning_scenario
+        self._learning_scenarios = learning_scenarios
 
     def learn(self)->None:
         self._learning_scenario.learn()
