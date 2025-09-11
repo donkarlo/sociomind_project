a040ba1dbf4095db79ed0e070d12db655fd30af4
Mohammad Rahmani <mohammad.rahmani.xyz@gmail.com>
2025-09-11 22:12:35 +0200

    scenario updated

--- src/sociomind/experiments/scenario.py
+++ src/sociomind/experiments/scenario.py
@@ -1,12 +1,8 @@
-from robotix.goal.goal import Goal
-from robotix.type.uav.quad_copter.quad_copter import QuadCopter
-from sensorx.obs.sensor_set_obss import SensorSetObss
-class Scenario:
-    def __init__(self, obss:SensorSetObss , quad_copters:list[QuadCopter,...]):
+from roboticx.
+
+class Scenario(RoboticxScenario):
+    def __init__(self):
         pass
 
     def learn(self)->None:
-        pass
-
-    def achieve(self, goals:tuple[Goal,...]) -> None:
         pass
