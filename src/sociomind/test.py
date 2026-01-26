63b4851ae758abf9a53c6c66d13fdd245fdec24c
Mohammad Rahmani <mohammad.rahmani.xyz@gmail.com>
2026-01-26 20:41:27 +0100

    nothing much

--- src/sociomind/test.py
+++ src/sociomind/test.py
@@ -1,23 +1,7 @@
-import matplotlib
-matplotlib.use("QtAgg")
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Example data
-x1 = np.array([0, 1, 2, 3])
-y1 = np.array([1, 2, 1, 2])
-
-x2 = np.array([0, 1, 2, 3])
-y2 = np.array([2, 1, 2, 1])
-
-fig, ax = plt.subplots()
-
-# Scatter points for both datasets
-ax.scatter(x1, y1)
-ax.scatter(x2, y2)
-
-# Connect corresponding points
-for i in range(len(x1)):
-    ax.plot([x1[i], x2[i]], [y1[i], y2[i]])
-
+x = np.linspace(0, 10, 100)
+plt.plot(x, np.sin(x))
+plt.title("Test Plot")
 plt.show()
